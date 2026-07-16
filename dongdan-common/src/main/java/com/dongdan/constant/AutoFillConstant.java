2a5203e9eac9070a31d2deadb301eddb813e421b
shijiu <2084485194@qq.com>
2026-07-16 20:49:26 +0800

    完善注释

--- dongdan-common/src/main/java/com/dongdan/constant/AutoFillConstant.java
+++ dongdan-common/src/main/java/com/dongdan/constant/AutoFillConstant.java
@@ -5,7 +5,7 @@ package com.dongdan.constant;
  */
 public class AutoFillConstant {
     /**
-     * 实体类中的方法名称
+     * 实体类中的方法名称, 严格区分大小写, 不能有空格
      */
     public static final String SET_CREATE_TIME = "setCreateTime";
     public static final String SET_UPDATE_TIME = "setUpdateTime";
